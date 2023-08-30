c4de06437705edee42eb385509adcd7d7209c068
Helen Ma <hhma@alumni.cpp.edu>
2023-08-29 22:03:53 -0400

    add a test line

--- 81-maven/src/main/java/com/in28minutes/learnmaven/LearnMavenApplication.java
+++ 81-maven/src/main/java/com/in28minutes/learnmaven/LearnMavenApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//Test commit
 public class LearnMavenApplication {
 
 	public static void main(String[] args) {
