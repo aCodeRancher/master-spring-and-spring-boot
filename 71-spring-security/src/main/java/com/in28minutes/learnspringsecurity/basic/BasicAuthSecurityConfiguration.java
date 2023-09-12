b0fa5e36d80d971028ab33be5d78bdc48b407c75
Helen Ma <hhma@alumni.cpp.edu>
2023-09-11 21:38:10 -0400

    comment out basic auth for now.

--- 71-spring-security/src/main/java/com/in28minutes/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
+++ 71-spring-security/src/main/java/com/in28minutes/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
@@ -2,6 +2,8 @@ package com.in28minutes.learnspringsecurity.basic;
 
 import javax.sql.DataSource;
 
+import com.in28minutes.learnspringsecurity.service.UserInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
@@ -15,14 +17,15 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 
-@Configuration
-@EnableMethodSecurity(jsr250Enabled = true, securedEnabled = true)
+//@Configuration
+//@EnableMethodSecurity(jsr250Enabled = true, securedEnabled = true)
 public class BasicAuthSecurityConfiguration {
 	
 	@Bean
@@ -54,23 +57,23 @@ public class BasicAuthSecurityConfiguration {
 		return http.build();
 	}
 
-//	@Bean
-//	public UserDetailsService userDetailService() {
-//		
-//		var user = User.withUsername("in28minutes")
-//			.password("{noop}dummy")
-//			.roles("USER")
-//			.build();
-//
-//		
-//		var admin = User.withUsername("admin")
-//				.password("{noop}dummy")
-//				.roles("ADMIN")
-//				.build();
-//
-//		return new InMemoryUserDetailsManager(user, admin);
-//	}
-	
+/*	@Bean
+	public UserDetailsService userDetailService() {
+
+		var user = User.withUsername("in28minutes")
+			.password("{noop}dummy")
+			.roles("USER")
+			.build();
+
+
+		var admin = User.withUsername("admin")
+				.password("{noop}dummy")
+				.roles("ADMIN")
+				.build();
+
+	return new InMemoryUserDetailsManager(user, admin);
+	}
+	*/
 	@Bean
 	public DataSource dataSource() {
 		return new EmbeddedDatabaseBuilder()
@@ -78,11 +81,12 @@ public class BasicAuthSecurityConfiguration {
 				.addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
 				.build();
 	}
-	
+
+
 	@Bean
 	public UserDetailsService userDetailService(DataSource dataSource) {
 		
-		var user = User.withUsername("in28minutes")
+		/*var user = User.withUsername("in28minutes")
 			//.password("{noop}dummy")
 			.password("dummy")
 			.passwordEncoder(str -> passwordEncoder().encode(str))
@@ -100,9 +104,10 @@ public class BasicAuthSecurityConfiguration {
 		jdbcUserDetailsManager.createUser(user);
 		jdbcUserDetailsManager.createUser(admin);
 
-		return jdbcUserDetailsManager;
+		return jdbcUserDetailsManager;*/
+		return new UserInfoService();
 	}
-	
+
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
