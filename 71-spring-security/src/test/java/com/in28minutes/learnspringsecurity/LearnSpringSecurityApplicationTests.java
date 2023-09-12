bb705d085a0547b8a41abb3b1373ed144c36a243
Helen Ma <hhma@alumni.cpp.edu>
2023-09-11 21:32:49 -0400

    gnerate a sample encoded password using Bcrypt algorithm

--- 71-spring-security/src/test/java/com/in28minutes/learnspringsecurity/LearnSpringSecurityApplicationTests.java
+++ 71-spring-security/src/test/java/com/in28minutes/learnspringsecurity/LearnSpringSecurityApplicationTests.java
@@ -2,12 +2,16 @@ package com.in28minutes.learnspringsecurity;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootTest
 class LearnSpringSecurityApplicationTests {
 
 	@Test
 	void contextLoads() {
+		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+		String encoded = bCryptPasswordEncoder.encode("dummy");
+		System.out.println("encoded " + encoded);
 	}
 
 }
