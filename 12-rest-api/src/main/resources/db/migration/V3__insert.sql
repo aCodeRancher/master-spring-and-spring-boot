008f442a067f1ea990b2e235cbfb70583f9a3494
Helen Ma <hhma@alumni.cpp.edu>
2024-01-29 22:06:49 -0500

    add a user and a new post.

--- 12-rest-api/src/main/resources/db/migration/V3__insert.sql
+++ 12-rest-api/src/main/resources/db/migration/V3__insert.sql
@@ -1,3 +1,6 @@
 insert into user_details ( id, name,birth_date ) values ( 1, 'Ranga','2000-10-02');
 insert into post (id, description ,user_id) values (1, 'I want to learn AWS', 1);
+insert into user_details ( id, name,birth_date ) values ( 2, 'Rao','2000-11-02');
+insert into post (id, description ,user_id) values (2, 'I want to learn Spring framework', 2);
+
 
